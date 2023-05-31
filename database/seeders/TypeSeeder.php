dba6b97f00f5c2e782388929d6dfdc98c6a96ffc
soniagaber <soniagaber90@gmail.com>
2023-05-31 18:24:23 +0200

    creata la migration per la tabella types

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,8 +16,16 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $types=['Back End', 'Front End', 'Full Stack', 'UX', 'UI'];
+        foreach($types as $type){
+            $newType= new Type();
+            $newType->name =$type;
+            $newType->slug = Str::slug($newType->name, '-');
+            $newType->description =$faker->text(100);
+
+            $newType->save();
+        }
     }
 }
